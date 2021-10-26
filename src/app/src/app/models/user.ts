ca40c56903df15c0b1345d332db7f14b9d83c105
Jose Veliz <josecarlosvelizcastro@gmail.com>
2021-10-26 12:09:56 -0600

    Prueba inicial de conexión de registro

--- src/app/src/app/models/user.ts
+++ src/app/src/app/models/user.ts
@@ -1,2 +1,18 @@
 export interface User {
+    username:string,
+    name:string,
+    password:string,
+    latitud:string,
+    longitud:string
+}
+
+export interface UserQuerie {
+    username:string,
+    latitud:string,
+    longitud:string
+}
+
+export interface User_Login {
+    username:string,
+    password:string
 }
--- src/app/src/app/services/user-service/user-service.service.ts
+++ src/app/src/app/services/user-service/user-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { environment } from "../../../../../environments/environment";
+import { User, UserQuerie, User_Login } from "../../models/user";
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registerUser(user: User){
+    return this.http.post(`${environment.apiUrl}register`, user);
+  }
+
+  login(user_login: User_Login){
+    return this.http.post(`${environment.apiUrl}login`, user_login);
+  }
+  
+  currentWeather(user_querie: UserQuerie){
+    return this.http.post(`${environment.apiUrl}currentweather`, user_querie);
+  }
 }
