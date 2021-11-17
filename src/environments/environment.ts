26ea5b1b338c331b491e31d188e85fea1f4884a1
Jose Veliz <josecarlosvelizcastro@gmail.com>
2021-11-17 00:31:02 -0600

    API Changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://35.229.71.118:5000/',
-  mapboxKey: 'pk.eyJ1IjoiamFjZXZlbDk3IiwiYSI6ImNrdjk1M2tpYjA4ZXYyb3MxeXZhY252MW0ifQ.khghgKdBtXT7ZaIBeqluIg'
+  mapboxKey: 'pk.eyJ1IjoiamFjZXZlbDk3IiwiYSI6ImNrdzJwdjV5aTBlcGIyd21uY3JlYThzZzAifQ.eHy7cwD5Zu5G6AMI_-Y-oA'
 };
 
 /*
